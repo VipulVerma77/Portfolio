412d0d3174187e0d5015d664ec238dee60d2c687
VipulVerma77 <vipulverma4577@gmail.com>
2025-04-21 20:08:40 +0530

    glow card

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Button from "./Button";
 import HeroExperience from "./models/hero_models/HeroExperience";
 import AnimatedCounter from "./AnimatedCounter";
 import NavBar from "./Navbar";
-import GlowCard from "./GLowCard";
+import GlowCard from "./GLowCard.jsx";
 import TitleHeader from "./TileHeader";
 import TechIconCardExperience from "./models/tech_logos/TechIconCardExperience";
 import ContactExperience from "./models/contact/ContactExperience";
