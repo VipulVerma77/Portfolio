6b81edf33ac75d13dcd00865c1c23ff6b5c00022
VipulVerma77 <vipulverma4577@gmail.com>
2025-04-21 20:11:59 +0530

    done may be

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-import Button from "./Button";
-import HeroExperience from "./models/hero_models/HeroExperience";
-import AnimatedCounter from "./AnimatedCounter";
-import NavBar from "./Navbar";
-import GlowCard from "./GLowCard.jsx";
-import TitleHeader from "./TileHeader";
-import TechIconCardExperience from "./models/tech_logos/TechIconCardExperience";
-import ContactExperience from "./models/contact/ContactExperience";
+import Button from "./Button.jsx";
+import HeroExperience from "./models/hero_models/HeroExperience.jsx";
+import AnimatedCounter from "./AnimatedCounter.jsx";
+import NavBar from "./Navbar.jsx";
+import GlowCard from "./GlowCard.jsx";
+import TitleHeader from "./TileHeader.jsx";
+import TechIconCardExperience from "./models/tech_logos/TechIconCardExperience.jsx";
+import ContactExperience from "./models/contact/ContactExperience.jsx";
 export {Button,HeroExperience,AnimatedCounter,NavBar,GlowCard,TitleHeader,TechIconCardExperience,ContactExperience};
