1af16aa84e3edc219d1f74746dad24f2661736da
VipulVerma77 <vipulverma4577@gmail.com>
2025-05-04 16:47:33 +0530

    logo change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <NavBar/>
       <Hero/>
       <AppShowcase/>
-      {/* <LogoShowcase/> */}
+      <LogoShowcase/>
       <FeatureCards/>
       <Experience/>
       <TechStack/>
